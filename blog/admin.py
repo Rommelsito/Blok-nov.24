1680e69ea02a10bfad44a7479ba1dd4009007674
Rommel <momerg2016@gmail.com>
2024-11-30 10:07:16 -0400

    update post

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,6 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
    list_display = ('title', 'created_at',"author")
-   list_filter = ("author","create_at")
-   search_fields = ('title', 'content',"author")
-
+   list_filter = ("author","created_at")
+   search_fields = ('title', 'content',"author__username")
 admin.site.register(Post,PostAdmin)
