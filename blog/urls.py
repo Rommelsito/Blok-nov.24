2df4fea7abad8dca0806b6e396313efbacb4bd84
Rommel <momerg2016@gmail.com>
2024-12-07 11:17:46 -0400

    delete-done

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView,PostDetailView,PostCreateView
+from .views import PostListView,PostDetailView
+from .views import PostCreateView,PostUpdateView,PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-new'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name= 'post-delete')
 ] 
 
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView #Otra forma de importar todo es con *
 from.models import Post
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = ('post_detail.html')
+    template_name = 'post_detail.html'
 
 class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content','author']
     template_name = 'Post_New.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title','content']
+    template_name = 'post_update.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('blog-home')
